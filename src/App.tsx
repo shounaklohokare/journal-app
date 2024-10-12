0196dee09b6370601f614acdbd8765d821c79a16
Shounak Lohokare <shounaklohokare@gmail.com>
2024-10-12 22:40:53 +0530

    added navbar

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { FC } from "react"
 
 const App: FC = () => {
 
-  return <div>
-    <h1>Journal App</h1>
+  return <div className="flex justify-center items-center absolute top-0 w-full h-24 bg-[#2a2a2a]">
+    <h1 className="text-4xl font-mono md:tracking-wider text-nowrap text-[#f3c278]">Journal App</h1>
   </div>
 
 }
