aab7648227994323d446357047d9d8bd39b7c26e
Shounak Lohokare <shounaklohokare@gmail.com>
2024-11-02 19:18:42 +0530

    made design changes

--- src/components/JournalEntry.tsx
+++ src/components/JournalEntry.tsx
@@ -8,12 +8,12 @@ const JournalEntry: FC<Entry> = ({ entry_id, created, title, content, updated })
 
     const navigate = useNavigate();
 
-    return <div className="mx-auto my-5 cursor-pointer" onClick={() => { navigate(`entry/${entry_id}`) }}>
-        <div className="flex justify-between">
+    return <div className="mx-auto my-5 space-y-1 cursor-pointer" onClick={() => { navigate(`entry/${entry_id}`) }}>
+        <div className="flex space-x-80">
             <div className="font-semibold">{title}</div>
             <div className="font-semibold">{formatDate(created)}</div>
         </div>
-        <div>{content}</div>
+        <div className="w-full">{content}</div>
     </div>
 
 }
