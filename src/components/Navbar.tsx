532d7af36a99e3c26abc93861a212d0cbd1fb74f
Shounak Lohokare <shounaklohokare@gmail.com>
2024-11-07 16:15:32 +0530

    added logout

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,20 +1,36 @@
 import { FC } from "react"
 import { useLocation, useNavigate } from "react-router-dom";
 import { IoCreateOutline } from "react-icons/io5";
+import { CiLogout } from "react-icons/ci";
+import { setAuthenticated, setUsername } from "../store/features/entrySlice";
+import { useAppDispatch } from "../store/store";
 
 const Navbar:FC = () => {
 
     const location = useLocation();
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+
+    const dispatch = useAppDispatch()
+
+    const handleLogOut = () => {
+        
+        dispatch(setAuthenticated(false))
+        dispatch(setUsername(""))
+        navigate("/")
+    }
 
 
     return (location.pathname === "/" ? <div className="flex justify-center flex-shrink-0 items-center absolute top-0 w-full h-24 font-mono bg-[#2a2a2a] text-[#f3c278]">
     <h1 className="text-[2.6rem] mx-72  md:tracking-wider text-nowrap  cursor-pointer">MindMemo</h1>
 </div> : (<div className="flex justify-between flex-shrink-0 items-center absolute top-0 w-full h-24 font-mono bg-[#2a2a2a] text-[#f3c278]">
         <h1 className="text-4xl mx-72  md:tracking-wider text-nowrap  cursor-pointer" onClick={() => { navigate("/home") }}>MindMemo</h1>
-        <div className="flex mx-64 space-x-2 cursor-pointer">
+        <div className="flex ml-96  space-x-2 cursor-pointer">
             <span className="pt-[0.08rem]"><IoCreateOutline size={30} /> </span>
             <span className="text-2xl" onClick={() => { navigate("/new_entry") }}>New Entry</span>
+        </div>
+        <div className="flex flex-col items-center pt-3 mx-12 space-y-1 cursor-pointer" onClick={handleLogOut}>
+        <CiLogout size={30}/>
+        <span className="text-sm">Logout</span>
         </div>
     </div>))
 
