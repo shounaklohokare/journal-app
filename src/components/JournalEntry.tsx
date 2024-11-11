762225e871df73a6e84c61c64c4cd93bf722b1af
Shounak Lohokare <shounaklohokare@gmail.com>
2024-11-11 11:48:31 +0530

    improved Tailwind CSS setup for the home page

--- src/components/JournalEntry.tsx
+++ src/components/JournalEntry.tsx
@@ -8,12 +8,12 @@ const JournalEntry: FC<Entry> = ({ entry_id, created, title, content, updated })
 
     const navigate = useNavigate();
 
-    return <div className="mx-auto my-5 space-y-1 cursor-pointer" onClick={() => { navigate(`/entry/${entry_id}`) }}>
-        <div className="flex space-x-80">
-            <div className="font-semibold">{title}</div>
+    return <div className="mx-auto my-5 space-y-2 cursor-pointer w-3/4 text-wrap" onClick={() => { navigate(`/entry/${entry_id}`) }}>
+        <div className="flex justify-between">
+            <div className="font-semibold w-2/3 truncate">{title}</div>
             <div className="font-semibold">{formatDate(created)}</div>
         </div>
-        <div className="w-full">{content}</div>
+        <div className="w-full truncate">{content}</div>
     </div>
 
 }
